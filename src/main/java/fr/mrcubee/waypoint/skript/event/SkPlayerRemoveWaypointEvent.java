e725dc855db3fda6d956b725042a635647bed02c
MrCubee <contact@mrcubee.fr>
2022-07-16 16:07:05 +0200

    Remove useless imports.

--- src/main/java/fr/mrcubee/waypoint/skript/event/SkPlayerRemoveWaypointEvent.java
+++ src/main/java/fr/mrcubee/waypoint/skript/event/SkPlayerRemoveWaypointEvent.java
@@ -1,8 +1,5 @@
 package fr.mrcubee.waypoint.skript.event;
 
-import ch.njol.skript.lang.Literal;
-import ch.njol.skript.lang.SkriptEvent;
-import ch.njol.skript.lang.SkriptParser;
 import org.bukkit.event.Event;
 
 public class SkPlayerRemoveWaypointEvent extends SkEvent {
