144a8543ece51c9d4f618c1ca77e95af17cfff4c
MrCubee <contact@mrcubee.fr>
2022-07-16 16:06:46 +0200

    Fix null pointer exception in get player waypoint expression.

--- src/main/java/fr/mrcubee/waypoint/skript/expression/SkGetPlayerWaypointExpression.java
+++ src/main/java/fr/mrcubee/waypoint/skript/expression/SkGetPlayerWaypointExpression.java
@@ -2,7 +2,6 @@ package fr.mrcubee.waypoint.skript.expression;
 
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
-import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
 import fr.mrcubee.waypoint.WayPoint;
 import fr.mrcubee.waypoint.WayPointStorage;
@@ -13,22 +12,18 @@ import java.util.Set;
 
 public class SkGetPlayerWaypointExpression extends SkExpression<WayPoint> {
 
-    private Expression<String> waypointNameExpression;
     private Expression<Player> targetPlayerExpression;
+    private Expression<String> waypointNameExpression;
 
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean kleenean, SkriptParser.ParseResult parseResult) {
         super.init(expressions, matchedPattern, kleenean, parseResult);
         if (expressions == null || expressions.length < 1)
             return false;
-        if (matchedPattern > 0) {
-            this.targetPlayerExpression = (Expression<Player>) expressions[0];
-            return true;
-        }
-        this.targetPlayerExpression = (Expression<Player>) expressions[1];
+        this.targetPlayerExpression = (Expression<Player>) expressions[0];
         if (expressions.length < 2)
-            return false;
-        this.waypointNameExpression = (Expression<String>) expressions[0];
+            return matchedPattern == 0;
+        this.waypointNameExpression = (Expression<String>) expressions[1];
         return true;
     }
 
@@ -51,7 +46,7 @@ public class SkGetPlayerWaypointExpression extends SkExpression<WayPoint> {
 
     @Override
     public boolean isSingle() {
-        return this.waypointNameExpression == null;
+        return this.waypointNameExpression != null;
     }
 
     @Override
--- src/main/java/fr/mrcubee/waypoint/skript/expression/WaypointSkriptExpressionRegister.java
+++ src/main/java/fr/mrcubee/waypoint/skript/expression/WaypointSkriptExpressionRegister.java
@@ -8,7 +8,7 @@ import fr.mrcubee.waypoint.WayPoint;
 public class WaypointSkriptExpressionRegister {
 
     public static void register() {
-        Skript.registerExpression(SkGetPlayerWaypointExpression.class, WayPoint.class, ExpressionType.COMBINED, "get %player%['s] waypoint[s]");
+        Skript.registerExpression(SkGetPlayerWaypointExpression.class, WayPoint.class, ExpressionType.COMBINED, "get %player%['s] waypoint[s]", "get %player%['s] %string% waypoint");
         Skript.registerExpression(SkGetPlayerWaypointNameExpression.class, String.class, ExpressionType.COMBINED, "get %player% waypoint['s|s'|s's] name[s]");
         Skript.registerExpression(SkCreateWaypointExpression.class, WayPoint.class, ExpressionType.COMBINED, "(new|create) %string% waypoint (at|from) %location%");
         SimplePropertyExpression.register(SkGetWaypointNameExpression.class, String.class, "name", "waypoint");
